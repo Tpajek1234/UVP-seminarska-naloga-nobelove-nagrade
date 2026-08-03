b9daf44a96bb50be4ab19184e458780270f25ec9
Tpajek1234 <tp01344@student.uni-lj.si>
2026-08-03 10:14:04 +0200

    preimenovanje datoteke pridobi

--- pridobi_podatke.py
+++ pridobi_podatke.py
@@ -1,7 +1,7 @@
 
 import requests
 import time
-
+import os
 
 leto=2025
 headers={"User-Agent":"Mozilla/5.0"}
@@ -16,19 +16,21 @@ teme_nagrada=[
 
 
 def koda_za_htmlje(leto):    
+    os.makedirs("htmlji", exist_ok=True)
+
     for stevilo in range(1901, leto + 1):
         for tema in teme_nagrada:
-            url='https://www.nobelprize.org/prizes/'+ tema +'/'+ str(stevilo) +'/summary/' #koda za pobiranje
+            url = 'https://www.nobelprize.org/prizes/'+ tema +'/'+ str(stevilo) +'/summary/' #koda za pobiranje
                 
     
             try:
-                ime_dat=f'{tema},{stevilo}.html'
+                ime_dat = f'{tema},{stevilo}.html'
                 odgovor = requests.get(url, headers=headers, timeout=10)
                 odgovor.raise_for_status()
 
                 vsebina = odgovor.text
 
-                with open(ime_dat, 'w', encoding='utf-8') as f:
+                with open(os.path.join("htmlji", ime_dat), 'w', encoding='utf-8') as f:
                      f.write(vsebina)        
                 print(f'Datoteka nobelova nagrada {ime_dat} ustvarjena')
 
@@ -38,7 +40,7 @@ def koda_za_htmlje(leto):
             time.sleep(2)
 
     
-koda_za_htmlje(leto)
+#koda_za_htmlje(leto)
 
 
 
