0c8a1f4e0243cb8f5df2420bd5e5bc3097d0fb26
Tpajek1234 <tp01344@student.uni-lj.si>
2026-08-05 19:32:56 +0200

    Ustvarjena glavna datoteka python za pogon vseh datotek

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import os
+import pridobi_podatke
+import izlusci
+import izlusci_nagrajence
+
+leto = 2025
+if not os.path.exists('htmlji'):
+    pridobi_podatke.koda_za_htmlje(leto)
+
+if not os.path.exists('nobelovi_nagrajenci'):
+    izlusci.pobere_o_nagrajencih(izlusci.izlusci_vse_povezave_nagrajencev())
+
+
+
+izlusci_o_nagraj = izlusci_nagrajence.po_datot_z_nagrjajenci()
+izlusci_nagrajence.shrani_v_csv(izlusci_o_nagraj)
